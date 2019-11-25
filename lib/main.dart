4f0a1e37a750baa45436006e59832ff0a9c976dc
Yuri Toledo <yuriwtoledo@gmail.com>
2019-11-25 07:08:26 -0300

    Created AppBar and TextSearch!

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gif_search/ui/home.dart';
 
-void main() => runApp(Home());
+void main() => runApp(MaterialApp(
+      theme: ThemeData(hintColor: Colors.white),
+      home: Home(),
+    ));
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,11 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
-final urlTrending =
-    'https://api.giphy.com/v1/gifs/trending?api_key=wfCaDWTY5cyMHapDmXGpnXiKXWdLloKQ&limit=20&rating=G';
-final urlSearch = '';
+import 'package:http/http.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -14,23 +10,57 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   String _search;
+  int _offset = 0;
 
   @override
   void initState() {
     super.initState();
 
-    this._getGifs();
+    _getGifs();
   }
 
   _getGifs() async {
-    final url = _search == null ? urlTrending : urlSearch;
+    String url;
 
-    http.Response response = await http.get(url);
+    if (_search == null) {
+      final urlTrending =
+          'https://api.giphy.com/v1/gifs/trending?api_key=wfCaDWTY5cyMHapDmXGpnXiKXWdLloKQ&limit=20&rating=G';
+      url = urlTrending;
+    } else {
+      final urlSearch =
+          'https://api.giphy.com/v1/gifs/search?api_key=wfCaDWTY5cyMHapDmXGpnXiKXWdLloKQ&q=$_search&limit=25&offset=$_offset&rating=G&lang=en';
+      url = urlSearch;
+    }
+
+    Response response = await get(url);
     return jsonDecode(response.body);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.network(
+          'https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif',
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        color: Colors.black,
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Busque aqui',
+                labelStyle: TextStyle(color: Colors.white),
+                border: OutlineInputBorder(),
+              ),
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
