ad2dbedd32b30a8c240008e209f70fc10687347b
Yuri Toledo <yuriwtoledo@gmail.com>
2019-11-27 07:09:17 -0300

    adding some theme!

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:gif_search/ui/home.dart';
 
 void main() => runApp(MaterialApp(
-      theme: ThemeData(hintColor: Colors.white),
+      theme: ThemeData(
+          hintColor: Colors.white,
+          iconTheme: IconThemeData(color: Colors.white),
+          textTheme: TextTheme(body1: TextStyle(color: Colors.white))),
       home: Home(),
     ));
