671fa3a7f16e44d0f10e4923857fe201a2178e3a
Yuri Toledo <yuriwtoledo@gmail.com>
2019-11-27 07:09:41 -0300

    Adding list, get more and a lot of new things learned :grin:

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -36,6 +36,11 @@ class _HomeState extends State<Home> {
     return jsonDecode(response.body);
   }
 
+  _getListLength(List data) {
+    final length = _search == null ? data.length : data.length + 1;
+    return length;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,6 +56,10 @@ class _HomeState extends State<Home> {
         child: Column(
           children: <Widget>[
             TextField(
+              onSubmitted: (text) => setState(() {
+                _search = text;
+                _offset = 0;
+              }),
               decoration: InputDecoration(
                 labelText: 'Busque aqui',
                 labelStyle: TextStyle(color: Colors.white),
@@ -86,6 +95,39 @@ class _HomeState extends State<Home> {
   }
 
   Widget _gifBuilder(BuildContext context, AsyncSnapshot snapshot) {
-    return Container();
+    final list = List.from(snapshot.data['data']);
+    return GridView.builder(
+      padding: EdgeInsets.all(10.0),
+      itemCount: _getListLength(list),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+      itemBuilder: (context, index) {
+        if (_search == null || index < list.length) {
+          return GestureDetector(
+            child: Image.network(
+              list[index]['images']['fixed_height']['url'],
+              height: 300.0,
+              fit: BoxFit.cover,
+            ),
+          );
+        } else {
+          return GestureDetector(
+            onTap: () => setState(() => _offset += 19),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.add,
+                  size: 28.0,
+                ),
+                Text(
+                  'Carregar mais',
+                )
+              ],
+            ),
+          );
+        }
+      },
+    );
   }
 }
